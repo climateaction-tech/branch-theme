0d7132be615124be6b665b40d36e61c57b84fe4a
Jack Lenox <jack@automattic.com>
2020-10-05 15:00:31 +0100

    Bump version number

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '0.0.2' );
+	define( '_S_VERSION', '0.0.3' );
 }
 
 if ( ! function_exists( 'branch_setup' ) ) :
