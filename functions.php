0d9b03491a196104650773f2d3bb6b46b59b2628
Jack Lenox <jack@low-carbon.digital>
2020-10-14 17:47:50 +0100

    Update styles

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '0.0.9' );
+	define( '_S_VERSION', '0.1.0' );
 }
 
 if ( ! function_exists( 'branch_setup' ) ) :
