b7f1e68344893d886b8002a900aa648e6458beba
hanopcan <hannah@opcan.co.uk>
2021-05-01 16:20:07 +0100

    Change custom taxonomy labels

--- inc/custom-posts-and-taxonomies.php
+++ inc/custom-posts-and-taxonomies.php
@@ -1,28 +1,43 @@
 <?php
-/** Custom posts and custom taxonomies definitions and customisations.
+/**
+ * Custom posts and custom taxonomies definitions.
  *
  *  @package Branch
  */
 
 add_action( 'init', 'branch_create_issue_taxonomy' );
 
-add_filter( 'post_link', 'issue_permalink', 10, 3 );
-add_filter( 'post_type_link', 'issue_permalink', 10, 3 );
 
 /**
  * Create the new 'issue' taxonomy if we don't already have it.
  */
 function branch_create_issue_taxonomy() {
 	if ( ! taxonomy_exists( 'issue' ) ) {
+
+		$labels = array(
+			'name'              => _x( 'Issues', 'taxonomy general name', 'branch' ),
+			'singular_name'     => _x( 'Issue', 'taxonomy singular name', 'branch' ),
+			'search_items'      => __( 'Search issues', 'branch' ),
+			'all_items'         => __( 'All issues', 'branch' ),
+			'parent_item'       => __( 'Parent issue', 'branch' ),
+			'parent_item_colon' => __( 'Parent issue:', 'branch' ),
+			'edit_item'         => __( 'Edit issue', 'branch' ),
+			'update_item'       => __( 'Update issue', 'branch' ),
+			'add_new_item'      => __( 'Add new issue', 'branch' ),
+			'new_item_name'     => __( 'New issue name', 'branch' ),
+			'menu_name'         => __( 'Issues', 'branch' ),
+		);
+
 		register_taxonomy(
 			'issue',
 			'post',
 			array(
 				'hierarchical' => true,
-				'label'        => __( 'Issues' ),
+				'label'        => __( 'Issues', 'branch' ),
+				'labels'       => $labels,
 				'public'       => true,
 				'show_ui'      => true,
-				'query_var'    => 'rating',
+				'query_var'    => 'issue',
 				'rewrite'      => true,
 			)
 		);
@@ -30,34 +45,3 @@ function branch_create_issue_taxonomy() {
 }
 
 
-/**
- * Amend all post permalinks to include the issue taxonomy in the URL.
- *
- * @param string  $permalink The post's permalink.
- * @param WP_Post $post_id The post in question.
- * @param bool    $leavename Whether to keep the post name.
- */
-function issue_permalink( $permalink, $post_id, $leavename ) {
-
-	if ( strpos( $permalink, '%issue%' ) === false ) :
-		return $permalink;
-	endif;
-
-	// Get post.
-	$post = get_post( $post_id );
-
-	if ( ! $post ) :
-		return $permalink;
-	endif;
-
-	// Get taxonomy terms.
-	$terms = wp_get_object_terms( $post->ID, 'issue' );
-
-	if ( ! is_wp_error( $terms ) && ! empty( $terms ) && is_object( $terms[0] ) ) :
-		$taxonomy_slug = $terms[0]->slug;
-	else :
-		$taxonomy_slug = 'not-rated';
-	endif;
-
-	return str_replace( '%issue%', $taxonomy_slug, $permalink );
-}
