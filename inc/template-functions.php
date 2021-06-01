353846901c61a3641353c7e7c303ef0101414525
hanopcan <hannah@opcan.co.uk>
2021-06-01 13:52:12 +0100

    Issue archive page and related ACF fields

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -35,3 +35,22 @@ function branch_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'branch_pingback_header' );
+
+
+/**
+ * Generate responsive image output, including srcset attributes
+ *
+ * @param int $image_id Id of image we wish to generate srcset for.
+ * @param string $sizes String of sizes image will be displayed at.
+ */
+function branch_output_responsive_image_markup( $image_id, $sizes ) {
+
+	$img_src    = wp_get_attachment_image_url( $image_id, 'large' );
+	$img_srcset = wp_get_attachment_image_srcset( $image_id, 'large' );
+	$img_alt    = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
+	?>
+
+	<img loading="lazy" src="<?php echo esc_attr( $img_src ); ?>" srcset="<?php echo esc_attr( $img_srcset ); ?>" sizes="<?php echo esc_html( $sizes ) ?>" alt="<?php echo esc_html( $img_alt ); ?>">
+
+	<?php
+}
